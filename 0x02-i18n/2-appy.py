9983fc61b3992ff3ffff263ac5e1c185a04dc7ab
Evans <ivanz.corbie@gmail.com>
2025-05-21 03:49:28 +0000

    2-appy.py

--- 0x02-i18n/2-appy.py
+++ 0x02-i18n/2-appy.py
@@ -5,31 +5,32 @@ Selects locale based on request headers.
 """
 
 from flask import Flask
-from flask import render_template
 from flask import request
+from flask import render_template
 from flask_babel import Babel
 
 
 class Config(object):
     """
-    Configuration for Babel.
-    Sets supported languages and default locale/timezone.
+    Application configuration class
     """
-    LANGUAGES = ["en", "fr"]
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Wrap the application with Babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """
-    Select the best matching language based on the request.
+    Gets locale from request object
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -37,7 +38,7 @@ def get_locale() -> str:
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-    Render the index page with a welcome message.
+    Renders a basic html template
     """
     return render_template('2-index.html')
 
