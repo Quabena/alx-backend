9a2adb78d4e77b752fe453340a827f9b59a39815
Evans <ivanz.corbie@gmail.com>
2025-05-21 03:33:19 +0000

    2-appy.py

---
+++ 0x02-i18n/2-appy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel for i18n.
+Selects locale based on request headers.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configuration for Babel.
+    Sets supported languages and default locale/timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Select the best matching language based on the request.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Render the index page with a welcome message.
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
